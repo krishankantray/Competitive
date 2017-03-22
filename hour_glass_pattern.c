dd9d61953f969614afdc5a1be2fac17b7784c91f
krishankantray <krishankant_rai@rediffmail.com>
2017-03-23 00:06:09 +0530

    Create hour_glass_pattern.c

---
+++ hour_glass_pattern.c
@@ -0,0 +1,66 @@
+/*Sample Input
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 2 4 4 0
+0 0 0 2 0 0
+0 0 1 2 4 0
+
+Sample Output
+
+19
+
+Explanation
+
+contains the following hourglasses:
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  1       0       0       0
+1 1 1   1 1 0   1 0 0   0 0 0
+
+0 1 0   1 0 0   0 0 0   0 0 0
+  1       1       0       0
+0 0 2   0 2 4   2 4 4   4 4 0
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  0       2       4       4
+0 0 0   0 0 2   0 2 0   2 0 0
+
+0 0 2   0 2 4   2 4 4   4 4 0
+  0       0       2       0
+0 0 1   0 1 2   1 2 4   2 4 0
+*/
+
+
+
+
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int arr[6][6];
+    int temp= -63,sum=0;
+    for(int arr_i = 0; arr_i < 6; arr_i++){
+       for(int arr_j = 0; arr_j < 6; arr_j++){
+          
+          scanf("%d",&arr[arr_i][arr_j]);
+       }
+    }
+    
+    for(int i=0; i<4 ;i++ )
+        for(int j=0; j<4 ; j++)
+        {
+        sum =arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+        if(sum > temp)
+            temp=sum;
+    }
+    printf("%d", temp);
+    return 0;
+}
